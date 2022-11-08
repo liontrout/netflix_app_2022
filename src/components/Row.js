a742a46a6d4efe213d4ae07bfc39f479b7745931
liontrout <rlaansgur95@gmail.com>
2022-11-08 23:13:13 +0900

    third commit

--- src/components/Row.js
+++ src/components/Row.js
@@ -9,17 +9,30 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
-function Row({isLargeRow, title, id, fetchUrl}) {
+function Row({isLargeRow, title, fetchUrl}) {
   const [movies, setMovies] = useState([]);
   const [modalOpen, setModalOpen] = useState(false);
   const [movieSelected, setMovieSelected] = useState({});
 
-  
   const fetchMovieData = useCallback (async () => {
     const request = await axios.get(fetchUrl);
     // console.log(request);
     setMovies(request.data.results);
   }, [fetchUrl]);
+
+  /*
+    const fetchMovieData = useCallback (async () => {
+      const request = await axios.get(fetchUrl);
+      // console.log(request);
+
+      const movieId = request.data.results[
+        Math.floor(Math.random() * request.data.results.length + 0)
+      ].id;
+
+      const {data: movieDetail} = await axios.get(`${"movie" || "discover/movie" || "discover/tv" || "trending/all"}${"/" || "?"}${movieId}`, {params: {append_to_response: "videos"}});
+      setMovies(movieDetail);
+    }, [fetchUrl]);
+  */
   
   useEffect(() => {
     fetchMovieData();
@@ -78,6 +91,24 @@ function Row({isLargeRow, title, id, fetchUrl}) {
                 />
               </SwiperSlide>
             ))}
+            {/* {movies.map(movie => (
+              <SwiperSlide key={movie.id}>
+                <img
+                  className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                  src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                  alt={movie.title || movie.name || movie.original_name}
+                  onClick={() => handleClick(movie)}
+                />
+              </SwiperSlide>
+            ))} */}
+            {/* <SwiperSlide key={movies.id}>
+              <img
+                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movies.poster_path : movies.backdrop_path}`}
+                alt={movies.title || movies.name || movies.original_name}
+                onClick={() => handleClick(movies)}
+              />
+            </SwiperSlide> */}
           </div>
           {/* <div className="slider__arrow right">
             <span
--- src/routes/MainPage.js
+++ src/routes/MainPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import requests from "../api/requests";
 import Banner from "../components/Banner";
-import Row from "../components/Row";
+import Row from '../components/Row';
 
 function MainPage() {
   return (
